e192c0cc761fec42a0a84806669c72800b56878b
Frederik Schmidt <frederik.schmidt@orsay.com>
2022-02-01 22:24:00 +0100

    added mini batch and introduced epoch

--- src/executables/model_np.py
+++ src/executables/model_np.py
@@ -8,31 +8,51 @@ def deep_neural_network_np(
     X: np.array,
     Y: np.array,
     layers_dims: list,
-    alpha: float = 0.0075,
-    num_iterations: int = 3000,
+    optimizer: str = "MiniBatchGradientDescent",
+    alpha: float = 0.1,
+    epochs: int = 10,
 ):
     """
-    Trains the neural network using gradient descent.
+    Trains the neural network.
     :param X: input data, of size (784, m)
     :param Y: true labels, of size (10, m)
     :param layers_dims: network architecture of the neural network
+    :param optimizer: optimizer to use for learning
     :param alpha: learning rate
-    :param num_iterations: number of iterations of the optimization loop
+    :param epochs: number of iterations of the optimization loop
     :return: dict containing the learned weights W and biases b
     """
     np.random.seed(1)
     params = initialize_params(layers_dims)
 
-    for i in range(num_iterations):
-        AL, caches = forward_propagation(X, params)
-        grads = backward_propagation(AL, Y, caches)
-        params = update_params(params, grads, alpha)
+    if optimizer == "GradientDescent":
+        for epoch in range(epochs):
+            AL, caches = forward_propagation(X, params)
+            grads = backward_propagation(AL, Y, caches)
+            params = update_params(params, grads, alpha)
 
-        if i % 100 == 0 or i == num_iterations - 1:
+            if epoch % 100 == 0 or epoch == epochs - 1:
+                cost = compute_cross_entropy_cost(AL, Y)
+                preds, _ = predict(X, params)
+                accuracy = compute_accuracy(preds, Y)
+                print(f"--- Epoch: {epoch} ---")
+                print("Cost:", cost)
+                print("Accuracy:", accuracy)
+
+    elif optimizer == "MiniBatchGradientDescent":
+        mini_batches = random_mini_batches(X, Y, mini_batch_size=64)
+        for epoch in range(1, epochs):
+            for mini_batch in mini_batches:
+                X_mini, Y_mini = mini_batch
+                AL, caches = forward_propagation(X_mini, params)
+                grads = backward_propagation(AL, Y_mini, caches)
+                params = update_params(params, grads, alpha)
+
+            AL, _ = forward_propagation(X, params)
             cost = compute_cross_entropy_cost(AL, Y)
             preds, _ = predict(X, params)
             accuracy = compute_accuracy(preds, Y)
-            print(f"--- Iteration: {i} ---")
+            print(f"--- Epoch: {epoch} ---")
             print("Cost:", cost)
             print("Accuracy:", accuracy)
 
@@ -47,6 +67,7 @@ if __name__ == "__main__":
         X=X_train,
         Y=y_train,
         layers_dims=layers_dims,
+        optimizer="MiniBatchGradientDescent",
         alpha=0.1,
-        num_iterations=500,
+        epochs=10,
     )
